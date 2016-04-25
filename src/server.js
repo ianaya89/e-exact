73d593b7793ca8785a1ddbd2ac8fc9fcc4d3e841
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-04-25 14:43:41 -0300

    Setting up babel

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import config from './config/config';
+
+import express from 'express';
+//import mongoose from 'mongoose';
+
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+import routes from './routes/index';
+
+const port = config.PORT;
+const app = express();
+
+//require('./libraries/promisify-all')(['mongoose', 'jsonwebtoken', 'bcrypt']);
+
+//mongoose.connect(config.MONGODB_URL);
+
+//app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(morgan('tiny'));
+
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listenning on port ${port}`) );
+
+module.exports = app;
