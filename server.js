0ef83ca5004d78bfebf0ce007068f870f2fec62e
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-04-25 12:05:13 -0300

    Adding express hello world

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
